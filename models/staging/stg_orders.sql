b4651d93a416f5d09f422d6cda19122fec1ddf1d
Krishna Vemula <vemulakrishnamsbi@outlook.com>
2026-05-24 05:17:17 +0000

    developing stg orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+SELECT 
+-- from orders table
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+-- from raw customer
+c.customername,
+c.segment,
+c.country,
+-- from raw product
+p.category,
+p.productname,
+p.subcategory
+FROM  {{ ref('raw_orders') }} o
+LEFT JOIN {{ ref('raw_customer') }} c
+ON o.customerid = c.customerid
+LEFT JOIN {{ ref('raw_product') }} p
+ON o.productid = p.productid
